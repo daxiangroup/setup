6c9dba5858b1748c5aa5cebc4156bb07c499fd69
tylers <tylers@nationalfibre.net>
2013-09-30 21:07:52 -0400

    ini changes

--- makedeb-remotes.ini.php
+++ makedeb-remotes.ini.php
@@ -1,27 +1,42 @@
 <?php
 return array(
-    /*
-    'setup' => array(
-        'hydrogen' => array(
-            'url'   => 'https://github.com/daxiangroup/hydrogen/archive/master.zip',
-            'label' => 'hydrogen',
-            'tag'   => 'daxiangroup-master-1',
-            'type'  => 'setup',
-        ),
+    //-----[ Setup Packages ]
+    'hydrogen' => array(
+        'type'        => 'setup',
+        'url'         => 'https://github.com/daxiangroup/setup/archive/',
+        'label'       => 'hydrogen',
+        'tag'         => 'master',
+        'description' => 'Daxian Group HQ',
     ),
-    */
+    'lithium' => array(
+        'type'        => 'setup',
+        'url'         => 'https://github.com/daxiangroup/setup/archive/',
+        'label'       => 'lithium',
+        'tag'         => 'master',
+        'description' => 'Daxian Group Webserver - lithium',
+    ),
 
+    //-----[ Site Packages ]
     'daxiangroup-corporate' => array(
         'type'  => 'site',
         'url'   => 'https://github.com/daxiangroup/daxiangroup/archive/',
         'label' => 'daxiangroup',
         'tag'   => 'corporate-1',
+        'role'  => 'lithium',
     ),
     'adrienneskitchen' => array(
         'type'  => 'site',
         'url'   => 'https://github.com/daxiangroup/adrienneskitchen/archive/',
         'label' => 'adrienneskitchen',
         'tag'   => 'master',
+        'role'  => 'lithium',
         //'deps'  => 'apache2, php5, php-mysql',
+    ),
+    'thelastprophet' => array(
+        'type'  => 'site',
+        'url'   => 'https://github.com/daxiangroup/thelastprophet/archive/',
+        'label' => 'thelastprophet',
+        'tag'   => 'master',
+        'role'  => 'lithium',
     ),
 );
---
+++ makedeb-roleMapping.ini.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'lithium' => 'dgrp-setup-lithium',
+);
